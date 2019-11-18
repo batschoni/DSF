f6bf1cc0268509e5f4d87eb9606e84d44d8ff0d5
Kaspar Lichtsteiner <kaspar.lichtsteiner@student.unisg.ch>
2019-11-18 12:09:13 +0100

    Adding comments

--- Data_Handling.R
+++ Data_Handling.R
@@ -21,15 +21,13 @@ data_unique = data %>%  #keep only the newest duplicates; possible with last fun
   ungroup
 
 data_chains = data_unique %>% 
-  group_by(Owner.Name) %>% 
+  group_by(Owner.Name) %>%  #group by owner to see who owns more than one company
   summarise(count = n())
 
-data_chains$count[which(data_chains$count == 1)] = 0
-data_chains$count[which(data_chains$count != 0)] = 1
+data_chains$count[which(data_chains$count == 1)] = 0 #gives every owner the value 0 if only one shop owned
+data_chains$count[which(data_chains$count != 0)] = 1 #gives value 1 if > 1 shop owned
 
-data_unique = inner_join(data_unique, data_chains, by = "Owner.Name")
+data_unique = inner_join(data_unique, data_chains, by = "Owner.Name") #merges the data to basic data set
 
-chains = data_unique
-chains[which()]
 
 write.csv(data_unique, file = "Data/Unique_data_food_inspections.csv") #save data as csv
