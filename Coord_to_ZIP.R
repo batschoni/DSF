cfcce0386d3da6d04e00e00c30c9869d4871bddf
Kaspar Lichtsteiner <kaspar.lichtsteiner@student.unisg.ch>
2019-11-19 12:48:23 +0100

    Generalization

--- Coord_to_ZIP.R
+++ Coord_to_ZIP.R
@@ -5,9 +5,9 @@ library(rgdal)
 rm(list = ls())
 data_bnb = read.csv("/Users/kasparlichtsteiner/Downloads/AB_NYC_2019.csv")
 
-Coordinates = tibble(rep(1,48895))
+Coordinates = tibble(rep(1,nrow(data_bnb)))
 Coordinates = mutate(Coordinates, longitude = as.numeric(data_bnb$longitude), latitude = as.numeric(data_bnb$latitude))
-Coordinates = select_(Coordinates, -rep(1,48895))
+Coordinates = select_(Coordinates, -rep(1,nrow(data_bnb)))
 
 s = shapefile("data/geolocation/ZIP_CODE_040114.shp")
 pts <- Coordinates
@@ -20,6 +20,7 @@ pts <- spTransform(pts, proj4string(s))
 zip_where <- pts %over% s
 Coordinates = mutate(Coordinates, ZIP = zip_where$ZIPCODE)
 Coordinates = inner_join(Coordinates, data_bnb, by = c("longitude", "latitude"))
-# but since we fabricated data, not all will be in a zip code since
-# ny isn't a rectangle, so we remove the "bad" data
-zip_where <- zip_where[complete.cases(zip_where),]
+
+numbers = Coordinates %>% 
+  group_by(ZIP) %>% 
+  summarise(count = n())
